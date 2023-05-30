c0a42cbfb3b0552501082344c5f24fe27109a0af
mi nombre <desarrollowebaps@gmail.com>
2023-05-29 23:07:24 -0400

    01. Results_summary_component

---
+++ js/main.js
@@ -0,0 +1,40 @@
+let base = document.querySelector("#base")
+let template__main = document.querySelector("#template__main").content
+let fragmentTemp = document.createDocumentFragment()
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+    dataFech()
+})
+
+
+const dataFech = async () =>{
+    try{
+        let res = await fetch("data.json")
+        let data = await res.json()
+        //console.log(data)
+        
+        pintarElementos(data)
+
+    }catch(error){
+        console.log(" Error de carga en el JSON")
+    }
+}
+
+
+const pintarElementos = (data) =>{
+    data.forEach(element => {
+        console.log(element)
+        template__main.querySelector("#texto").textContent = element.category
+        template__main.querySelector("#number").textContent = element.score
+        template__main.querySelector("#iconos").setAttribute("src",element.icon)
+        template__main.querySelector("#iconos").setAttribute("alt",element.category)
+
+
+        let dataClone = template__main.cloneNode(true)
+        fragmentTemp.appendChild(dataClone)
+    });
+    base.appendChild(fragmentTemp)
+}
+
+
